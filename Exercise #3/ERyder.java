6089e15b0e4034806a4e676b90d00780c823c21c
Jiajietai <791716354@qq.com>
2026-03-07 11:18:06 +0800

    commiting9

--- Exercise #3/ERyder.java
+++ Exercise #3/ERyder.java
@@ -11,4 +11,26 @@ public class ERyder {
         this.isAvailable = isAvailable;
         this.kmDriven = kmDriven;
     }
+    
+    public ERyder() {}
+
+    public void ride() {
+        if (batteryLevel > 0 && isAvailable) {
+            System.out.println("the bike is available");
+        }
+        else {
+            System.out.println("the bike is not available");
+        }
+    }
+
+    public void printBikeDetails() {
+        System.out.println("Bike ID: " + bikeID);
+        System.out.println("Battery Level: " + batteryLevel);
+        System.out.println("Is Available: " + isAvailable);
+        System.out.println("KM Driven: " + kmDriven);
+    }
+
+    public String getBikeID() {
+        return bikeID;
+    }
 }
