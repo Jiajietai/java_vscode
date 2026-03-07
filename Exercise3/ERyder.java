9bea470ad5624ebfad0d42bb226727a50a1782f4
Jiajietai <791716354@qq.com>
2026-03-07 14:51:41 +0800

    commiting12

--- Exercise3/ERyder.java
+++ Exercise3/ERyder.java
@@ -1,18 +1,19 @@
 package Exercise3;
+
 public class ERyder {
     private String bikeID;
     private int batteryLevel;
-    private Boolean isAvailable;
+    private boolean isAvailable;
     private float kmDriven;
 
-    public ERyder(String bikeID, int batteryLevel, Boolean isAvailable, float kmDriven) {
+    public ERyder(String bikeID, int batteryLevel, boolean isAvailable, float kmDriven) {
         this.bikeID = bikeID;
         this.batteryLevel = batteryLevel;
         this.isAvailable = isAvailable;
         this.kmDriven = kmDriven;
     }
     
-    public ERyder() {}
+    public ERyder(){}
 
     public void ride() {
         if (batteryLevel > 0 && isAvailable) {
@@ -30,7 +31,34 @@ public class ERyder {
         System.out.println("KM Driven: " + kmDriven);
     }
 
+    public void setBikeID(String bikeID) {
+        this.bikeID = bikeID;
+        if (batteryLevel < 0 || batteryLevel > 100) {
+            System.out.println("the batteryLevel is not valid");
+        }
+    }
     public String getBikeID() {
         return bikeID;
+    }
+
+    public void setBatteryLevel(int batteryLevel) {
+        this.batteryLevel = batteryLevel;
+    }
+    public int getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setKmDriven(float kmDriven) {
+        this.kmDriven = kmDriven;
+    }
+    public float getKmDriven() {
+        return kmDriven;
     }
 }
--- Exercise3/Main.java
+++ Exercise3/Main.java
@@ -1,9 +1,16 @@
 package Exercise3;
+
 public class Main {
     public static void main(String[] args) {
-        ERyder bike = new ERyder("Bike1", 100, true, 10.1f);
+        ERyder bike = new ERyder();
+
+        bike.setBikeID("Bike01");
+        bike.setBatteryLevel(100);
+        bike.setIsAvailable(true);
+        bike.setKmDriven(10.1f);
 
         bike.ride();
         bike.printBikeDetails();
     }   
+
 }
