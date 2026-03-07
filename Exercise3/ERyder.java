89d51dfaf1e84b55cf0718219100a885addbe26a
Jiajietai <791716354@qq.com>
2026-03-07 13:22:39 +0800

    commiting11

---
+++ Exercise3/ERyder.java
@@ -0,0 +1,36 @@
+package Exercise3;
+public class ERyder {
+    private String bikeID;
+    private int batteryLevel;
+    private Boolean isAvailable;
+    private float kmDriven;
+
+    public ERyder(String bikeID, int batteryLevel, Boolean isAvailable, float kmDriven) {
+        this.bikeID = bikeID;
+        this.batteryLevel = batteryLevel;
+        this.isAvailable = isAvailable;
+        this.kmDriven = kmDriven;
+    }
+    
+    public ERyder() {}
+
+    public void ride() {
+        if (batteryLevel > 0 && isAvailable) {
+            System.out.println("the bike is available");
+        }
+        else {
+            System.out.println("the bike is not available");
+        }
+    }
+
+    public void printBikeDetails() {
+        System.out.println("Bike ID: " + bikeID);
+        System.out.println("Battery Level: " + batteryLevel);
+        System.out.println("Is Available: " + isAvailable);
+        System.out.println("KM Driven: " + kmDriven);
+    }
+
+    public String getBikeID() {
+        return bikeID;
+    }
+}
---
+++ Exercise3/Main.java
@@ -0,0 +1,9 @@
+package Exercise3;
+public class Main {
+    public static void main(String[] args) {
+        ERyder bike = new ERyder("Bike1", 100, true, 10.1f);
+
+        bike.ride();
+        bike.printBikeDetails();
+    }   
+}
