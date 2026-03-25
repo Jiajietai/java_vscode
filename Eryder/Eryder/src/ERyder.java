134847293ad2b737a13f713a52c2a0291bc3633a
Jiajietai <791716354@qq.com>
2026-03-25 23:07:44 +0800

    Ex7

---
+++ Eryder/Eryder/src/ERyder.java
@@ -0,0 +1,101 @@
+
+
+public class ERyder {
+
+    //private static final String COMPANY_NAME = "ERyder";
+    private static final double BASE_FARE = 1.0;
+    private static final double PER_MINUTE_FARE = 0.5;
+
+    private final String LINKED_ACCOUNT;
+    private final long LINKED_PHONE_NUMBER;
+
+    private String bikeID;
+    private int batteryLevel;
+    private boolean isAvailable;
+    private double kmDriven;
+    private int totalUsageInMinutes;
+    private double totalFare;
+
+    public ERyder(String bikeID, int batteryLevel, boolean isAvailable, double kmDriven) {
+        this.bikeID = bikeID;
+        this.batteryLevel = batteryLevel;
+        this.isAvailable = isAvailable;
+        this.kmDriven = kmDriven;
+        LINKED_ACCOUNT = "JIAJIETAI";
+        LINKED_PHONE_NUMBER = 1234567890;
+    }
+
+    public ERyder(String bikeID, int batteryLevel, boolean isAvailable, double kmDriven, String linkedAccount, long linkedPhoneNumber) {
+        this.bikeID = bikeID;
+        this.batteryLevel = batteryLevel;
+        this.isAvailable = isAvailable;
+        this.kmDriven = kmDriven;
+        LINKED_ACCOUNT = linkedAccount;
+        LINKED_PHONE_NUMBER = linkedPhoneNumber;
+    }
+
+    public void setBikeID(String bikeID) {
+        this.bikeID = bikeID;
+        if (batteryLevel < 0 || batteryLevel > 100) {
+            System.out.println("the batteryLevel is not valid");
+        }
+    }
+    public String getBikeID() {
+        return bikeID;
+    }
+
+    public void setBatteryLevel(int batteryLevel) {
+        this.batteryLevel = batteryLevel;
+    }
+    public int getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setKmDriven(float kmDriven) {
+        this.kmDriven = kmDriven;
+    }
+    public double getKmDriven() {
+        return kmDriven;
+    }
+
+
+    public void ride() {
+        if (batteryLevel > 0 && isAvailable) {
+            System.out.println("the bike is available");
+        }
+        else {
+            System.out.println("the bike is not available");
+        }
+    }
+
+    public void printBikeDetails() {
+        System.out.println("Bike ID: " + bikeID);
+        System.out.println("Battery Level: " + batteryLevel);
+        System.out.println("Is Available: " + isAvailable);
+        System.out.println("KM Driven: " + kmDriven);
+    }
+
+
+    public void printRideDetails(int usageInMinutes) {
+        System.out.println("Linked Account: " + LINKED_ACCOUNT);
+        System.out.println("Linked Phone Number: " + LINKED_PHONE_NUMBER);
+        System.out.println("Usage Time: " + usageInMinutes + " minutes");
+        System.out.println("Total Fare: " + totalFare);
+    }
+
+    private double calculateFare(int usageInMinutes) {
+        totalFare = BASE_FARE + (PER_MINUTE_FARE * usageInMinutes);
+        return totalFare;
+    }
+
+    public void getcalculateFare() {
+        calculateFare(totalUsageInMinutes);
+    }
+}
---
+++ Eryder/Eryder/src/Feedback.java
@@ -0,0 +1,76 @@
+
+
+public class Feedback {
+    
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String completeFeedback;
+    private String reviewID;
+    private boolean longFeedback;
+
+    public Feedback(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public void  analyseFeedback(boolean isConcatenation,String sent1, String sent2, String sent3, String sent4, String sent5){
+        if (isConcatenation) {
+            completeFeedback = feedbackUsingConcatenation(sent1, sent2, sent3, sent4, sent5);
+        } else {
+            completeFeedback = feedbackUsingStringBuilder(sent1, sent2, sent3, sent4, sent5);
+        }
+        checkFeedbackLength(completeFeedback);
+        creatReveiwID(firstName, lastName,completeFeedback);
+    }
+
+    private String feedbackUsingConcatenation(String sent1, String sent2, String sent3, String sent4, String sent5) {
+        String completeFeedback = sent1 + " " + sent2 + " " + sent3 + " " + sent4 + " " + sent5;
+        return completeFeedback;
+    }
+
+    private String feedbackUsingStringBuilder(String sent1, String sent2, String sent3, String sent4, String sent5) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(sent1).append(" ").append(sent2).append(" ").append(sent3).append(" ").append(sent4).append(" ").append(sent5);
+        return sb.toString();
+    }
+
+    private boolean checkFeedbackLength(String feedback) {
+        if (feedback.length() > 500) {
+            longFeedback = true;
+            return true;
+        } else {
+            longFeedback = false;
+            return false;
+        }
+    }
+
+    private String creatReveiwID(String firstName, String lastName, String completeFeedback) {
+        String reviewID = firstName+ lastName;
+        reviewID = reviewID.substring(2, 6);
+        reviewID = reviewID.toUpperCase();
+        reviewID = reviewID + completeFeedback;
+        reviewID = reviewID.substring(10, 15);
+        reviewID = reviewID + completeFeedback.length() + "_";
+        reviewID = reviewID + System.currentTimeMillis();
+        reviewID = reviewID.replaceAll(" ", "");
+        return reviewID;
+    }
+
+    @Override
+    public String toString() {
+        return "Feedback{" + "\n" + "firstName='" + firstName + "\n" + ", lastName='" + lastName + "\n" + ", email='" + email + "\n" + ", completeFeedback='" + completeFeedback + "\n" + ", reviewID='" + reviewID + "\n" + ", whether a longFeedback" + longFeedback +  "\n" +'}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+}
